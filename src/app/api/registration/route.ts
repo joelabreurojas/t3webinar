c19e16ca845db1b354e256bafc8207d953fd1ae1
Joel Abreu <joelabreurojas@gmail.com>
2024-06-25 23:37:33 -0400

    refactor(api/registration): change to smpt sender

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -1,28 +1,44 @@
-import { EmailTemplate } from "~/_components/email";
-import { Resend } from "resend";
 import { NextApiRequest, NextApiResponse } from "next/server";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export async function POST(request: NextApiRequest, res: NextApiResponse) {
   try {
-    const { name, email } = await request.json();
+    const { email, name } = await request.json();
 
-    const firstName = name.split(" ")[0];
+    const firstName =  name.split(" ")[0]
 
-    const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: [email],
-      subject: "Access to Webinar - Tech Talent Hunt",
-      react: EmailTemplate({ firstName }),
+    const transporter = nodemailer.createTransport({
+      service: process.env.SMTP_SERVICE,
+      host: process.env.SMTP_HOST,
+      secure: process.env.SMTP_SECURE,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+    const info = await transporter.sendMail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: "Tech Talent Hub - Access to Webinar",
+      html: `<div><h1>Hello, ${firstName}. Thanks for registering!</h1><p>Our meeting will be on July 10.</p><a href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link</a><p>We are glad you are interested in joining us.</p></div>`,
+    });
 
-    return Response.json(data);
+    return new Response(JSON.stringify({ message: "Email sent" }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    console.error("Error sending email:", error);
+
+    return new Response(JSON.stringify({ error: "Failed to send email" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 }
