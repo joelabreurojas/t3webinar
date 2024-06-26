28edc82bea3e667039331c961806a2c85ed39ee1
Joel Abreu <joelabreurojas@gmail.com>
2024-06-26 00:01:47 -0400

    refactor: update mail template

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: NextApiRequest, res: NextApiResponse) {
   try {
     const { email, name } = await request.json();
 
-    const firstName =  name.split(" ")[0]
+    const firstName = name.split(" ")[0];
 
     const transporter = nodemailer.createTransport({
       service: process.env.SMTP_SERVICE,
@@ -22,7 +22,63 @@ export async function POST(request: NextApiRequest, res: NextApiResponse) {
       from: process.env.SMTP_FROM,
       to: email,
       subject: "Tech Talent Hub - Access to Webinar",
-      html: `<div><h1>Hello, ${firstName}. Thanks for registering!</h1><p>Our meeting will be on July 10.</p><a href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link</a><p>We are glad you are interested in joining us.</p></div>`,
+      html: `
+        <head>
+          <link
+            href='https://fonts.googleapis.com/css?family=Inter'
+            rel='stylesheet'
+          >
+        </head>
+        <body style= 'background-color: white'>
+          <table style='
+            color: black;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 0;
+            border-collapse: collapse;
+            font-family:
+              &quot;Inter&quot;,
+              &quot;Helvetica&quot;,
+              &quot;Arial&quot;,
+              sans-serif;
+            '
+            height='100%'
+            width='100%'
+          >
+            <tbody>
+              <tr>
+                <td style='padding: 30px;' align='center' valign='top'>
+                  <div>
+                    <img
+                      style='text-align: center;'
+                      src='https://utfs.io/f/a6d4d414-e41e-462f-bf5f-bcb5fedf89df-2olo5d.png'
+                      alt='Tech Talent Hub notification'
+                      width='48'
+                    >
+                    <h1 style='
+                      font-size: 2.4em;
+                      line-height: 1.3;
+                      '
+                    >Hello, ${firstName}!</h1>
+                    <p style='font-size: 1.6em; line-height: 1.3'>
+                      Our meeting will be on July 10.
+                    </p>
+                    <p style='font-size: 1.6em; line-height: 1.3'>
+                      We are glad you are interested in joining us.
+                    </p>
+                    <hr>
+                    <p><strong>
+                      <a
+                        href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link
+                      </a>
+                    </strong></p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>
+      `,
     });
 
     return new Response(JSON.stringify({ message: "Email sent" }), {
