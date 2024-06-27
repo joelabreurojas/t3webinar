3cfafaf94d6f87cef66079145a63f094b83effb5
Joel Abreu <joelabreurojas@gmail.com>
2024-06-27 16:45:13 -0400

    refactor: list users to send reminders

--- src/app/api/send/reminder/route.ts
+++ src/app/api/send/reminder/route.ts
@@ -7,82 +7,79 @@ export async function GET() {
     const users = await db.query.users.findMany();
 
     if (!users.length) {
-      return new Response(JSON.stringify({ error: "No users found" }), {
-        status: 404,
+      return new Response(JSON.stringify({ message: "No users found, no emails sent" }), {
+        status: 200,
         headers: {
           "Content-Type": "application/json",
         },
       });
     }
 
-    users.map(
-      async (userData) =>
-        await sendEmail(
-          {
-            firstName: userData.name.split(" ")[0],
-            email: userData.email,
-          },
-          {
-            title: "Tech Talent Hub - Reminder to Webinar",
-            message: `
-            <head>
-              <link
-                href='https://fonts.googleapis.com/css?family=Inter'
-                rel='stylesheet'
-              >
-            </head>
-            <body style= 'background-color: white'>
-              <table style='
-                color: black;
-                max-width: 600px;
-                margin: 0 auto;
-                padding: 0;
-                border-collapse: collapse;
-                font-family:
-                  &quot;Inter&quot;,
-                  &quot;Helvetica&quot;,
-                  &quot;Arial&quot;,
-                  sans-serif;
-                '
-                height='100%'
-                width='100%'
-              >
-                <tbody>
-                  <tr>
-                    <td style='padding: 30px;' align='center' valign='top'>
-                      <div>
-                        <img
-                          style='text-align: center;'
-                          src='https://utfs.io/f/a6d4d414-e41e-462f-bf5f-bcb5fedf89df-2olo5d.png'
-                          alt='Tech Talent Hub notification'
-                          width='48'
-                        >
-                        <h1 style='
-                          font-size: 2.4em;
-                          line-height: 1.3;
-                          '
-                        >Hello again!</h1>
-                        <p style='font-size: 1.6em; line-height: 1.3'>
-                          Our event takes place today, don't miss it.
-                        </p>
-                        <p style='font-size: 1.6em; line-height: 1.3'>
-                          We are waiting for you.
-                        </p>
-                        <hr>
-                        <p><strong>
-                          <a
-                            href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link
-                          </a>
-                        </strong></p>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </body>
-          `,
-        }),
-    );
+    for (const userData of users) {
+      await sendEmail({
+        firstName: userData.name.split(" ")[0],
+        email: userData.email,
+      }, {
+        title: "Tech Talent Hub - Reminder to Webinar",
+        message: `
+          <head>
+            <link
+              href='https://fonts.googleapis.com/css?family=Inter'
+              rel='stylesheet'
+            >
+          </head>
+          <body style= 'background-color: white'>
+            <table style='
+              color: black;
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 0;
+              border-collapse: collapse;
+              font-family:
+                &quot;Inter&quot;,
+                &quot;Helvetica&quot;,
+                &quot;Arial&quot;,
+                sans-serif;
+              '
+              height='100%'
+              width='100%'
+            >
+              <tbody>
+                <tr>
+                  <td style='padding: 30px;' align='center' valign='top'>
+                    <div>
+                      <img
+                        style='text-align: center;'
+                        src='https://utfs.io/f/a6d4d414-e41e-462f-bf5f-bcb5fedf89df-2olo5d.png'
+                        alt='Tech Talent Hub notification'
+                        width='48'
+                      >
+                      <h1 style='
+                        font-size: 2.4em;
+                        line-height: 1.3;
+                        '
+                      >Hello again!</h1>
+                      <p style='font-size: 1.6em; line-height: 1.3'>
+                        Our event takes place today, don't miss it.
+                      </p>
+                      <p style='font-size: 1.6em; line-height: 1.3'>
+                        We are waiting for you.
+                      </p>
+                      <hr>
+                      <p><strong>
+                        <a
+                          href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link
+                        </a>
+                      </strong></p>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </body>
+        `,
+      });
+    }
 
     return new Response(JSON.stringify({ message: "Email sent" }), {
       status: 200,
