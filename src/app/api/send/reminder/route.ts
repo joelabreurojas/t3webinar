79a0054961bbd9f97ae6d6b1b4291c3057464577
Joel Abreu <joelabreurojas@gmail.com>
2024-06-27 15:51:47 -0400

    feat: add post handler for reminders

--- src/app/api/send/reminder/route.ts
+++ src/app/api/send/reminder/route.ts
@@ -101,3 +101,89 @@ export async function GET() {
     });
   }
 }
+
+export async function POST(request) {
+  try {
+    const userData = await request.json();
+    const firstName = userData.name.split(" ")[0];
+    const messageData = {
+      title: "Tech Talent Hub - Reminder to Webinar",
+      message: `
+        <head>
+          <link
+            href='https://fonts.googleapis.com/css?family=Inter'
+            rel='stylesheet'
+          >
+        </head>
+        <body style= 'background-color: white'>
+          <table style='
+            color: black;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 0;
+            border-collapse: collapse;
+            font-family:
+              &quot;Inter&quot;,
+              &quot;Helvetica&quot;,
+              &quot;Arial&quot;,
+              sans-serif;
+            '
+            height='100%'
+            width='100%'
+          >
+            <tbody>
+              <tr>
+                <td style='padding: 30px;' align='center' valign='top'>
+                  <div>
+                    <img
+                      style='text-align: center;'
+                      src='https://utfs.io/f/a6d4d414-e41e-462f-bf5f-bcb5fedf89df-2olo5d.png'
+                      alt='Tech Talent Hub notification'
+                      width='48'
+                    >
+                    <h1 style='
+                      font-size: 2.4em;
+                      line-height: 1.3;
+                      '
+                    >Hello again, ${firstName}!</h1>
+                    <p style='font-size: 1.6em; line-height: 1.3'>
+                      Our event takes place today, don't miss it.
+                    </p>
+                    <p style='font-size: 1.6em; line-height: 1.3'>
+                      We are waiting for you.
+                    </p>
+                    <hr>
+                    <p><strong>
+                      <a
+                        href='https://meet.google.com/xtk-hjor-eyq'>Google Meet Link
+                      </a>
+                    </strong></p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>
+      `,
+    };
+
+    await sendEmail(userData, messageData);
+
+    return new Response(JSON.stringify({ message: "Email sent" }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+
+    return new Response(JSON.stringify({ error: "Failed to send email" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
+
